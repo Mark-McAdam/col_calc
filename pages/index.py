6b6450e0221093503b89abc45f39b55ef351eb25
Mark McAdam <mark.a.mcadam@gmail.com>
2020-03-05 15:46:03 -0800

    pages uploaded

---
+++ pages/index.py
@@ -0,0 +1,43 @@
+# Imports from 3rd party libraries
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.express as px
+
+# Imports from this application
+from app import app
+
+# 2 column layout. 1st column width = 4/12
+# https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
+column1 = dbc.Col(
+    [
+        dcc.Markdown(
+            """
+        
+            ## Your Value Proposition
+
+            Estimate the cost of living, using easy to find information. 
+
+            COL CALC helps you determine the overall cost of living for a given area. We use easy to find information such as rent, cost of a movie ticket, and average utility cost to paint a clear overall idea. 
+
+
+            """
+        ),
+        dcc.Link(dbc.Button('Calc your COL', color='primary'), href='/predictions')
+    ],
+    md=4,
+)
+
+gapminder = px.data.gapminder()
+fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent",
+           hover_name="country", log_x=True, size_max=60)
+
+column2 = dbc.Col(
+    [
+        dcc.Graph(figure=fig),
+    ]
+)
+
+layout = dbc.Row([column1, column2])
